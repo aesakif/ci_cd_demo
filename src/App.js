5c4eebafabd5a50dd293402cb43539bc6fbf608c
aesakif <146798686+aesakif@users.noreply.github.com>
2024-03-04 12:17:53 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello, from nasir bro !!!!!!!!!!!!!!!! hello again!!~! -----------
+         Hello, new update
         </p>
         <a
           className="App-link"
